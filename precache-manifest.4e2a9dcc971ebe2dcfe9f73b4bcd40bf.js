de0e0937c9693b28730d247f51918874744d0d66
srinivasan <srinivasanorg@yahoo.com>
2020-07-29 22:42:38 +0530

    Updates

---
+++ precache-manifest.4e2a9dcc971ebe2dcfe9f73b4bcd40bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f10f12dfea0d9da5c6143ba0912296",
+    "url": "/Corono_Tracker/index.html"
+  },
+  {
+    "revision": "074ed138b6edb0222c0c",
+    "url": "/Corono_Tracker/static/css/main.4497d832.chunk.css"
+  },
+  {
+    "revision": "de1596fff340bbc204d5",
+    "url": "/Corono_Tracker/static/js/2.fbbd6963.chunk.js"
+  },
+  {
+    "revision": "bbe2dbbdaea8525fcc46934c5c58f863",
+    "url": "/Corono_Tracker/static/js/2.fbbd6963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074ed138b6edb0222c0c",
+    "url": "/Corono_Tracker/static/js/main.7e208a6a.chunk.js"
+  },
+  {
+    "revision": "e5abe3a02ec5a4073bc9",
+    "url": "/Corono_Tracker/static/js/runtime-main.f334c429.js"
+  }
+]);
